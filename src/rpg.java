e929366abdc3b0c5b7c269f3a9936b52c7bee973
DOMAIN\chunsing <chunsing.cheung@iaspec.com>
2022-11-29 09:28:40 +0800

    update

--- src/rpg.java
+++ src/rpg.java
@@ -7,72 +7,73 @@ class rpg {
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-        System.out.println("Hello, World!");
-
-        Unit player = new Unit(100, 10, 10, "Player");
-        Unit enemy = new Unit(100, 5, 5, "Enemy");
-        boolean userActionValid = false;
-        int counter = 1;
-
-        while (true) {
-            System.out.println("------Round " + counter + " Start------");
-            System.out.println(
-                    "Enemy HP: " + enemy.getHP() + " (ATK:" + enemy.getATK() + ", DEF:" + enemy.getDEF() + ")");
-            showHP(enemy.getHP(), enemy.getMaxHP());
-            System.out.println("--------------------");
-            System.out.println(
-                    "Player HP: " + player.getHP() + " (ATK:" + player.getATK() + ", DEF:" + player.getDEF() + ")");
-            showHP(player.getHP(), player.getMaxHP());
-            System.out.println("--------------------");
-            System.out.println("[0]: Attack");
-            System.out.println("[1]: Defence");
-            System.out.println("[2]: Power Up");
-
-            // user action
-            int userAction = -1;
-            while (!userActionValid) {
-                userAction = scan.nextInt();
-                System.out.print("\033[1F\33[K");
-                System.out.flush();
-                if ((userAction >= 0) && (userAction <= 2)) {
-                    userActionValid = true;
-                    System.out.print("\033[H\033[2J");
-                    System.out.flush();
-                    System.out.println("--------------------");
-                } else {
-                    System.out.print("\033[1F\33[K");
-                    System.out.flush();
-                    System.out.println("Invalid input!");
-                }
-            }
-            action(userAction, player, enemy);
-            if ((enemy.getHP() <= 0)) {
-                break;
-            }
-
-            // enemy action
-            int r = (int) (Math.random() * 3);
-            action(r, enemy, player);
-            if ((player.getHP() <= 0)) {
-                break;
-            }
-
-            userActionValid = false;
-            userAction = -1;
-            counter++;
+        try (Scanner scan = new Scanner(System.in)) {
             System.out.print("\033[H\033[2J");
             System.out.flush();
+            System.out.println("Hello, World!");
+
+            Unit player = new Unit(100, 10, 10, "Player");
+            Unit enemy = new Unit(100, 5, 5, "Enemy");
+            boolean userActionValid = false;
+            int counter = 1;
+
+            while (true) {
+                System.out.println("------Round " + counter + " Start------");
+                System.out.println(
+                        "Enemy HP: " + enemy.getHP() + " (ATK:" + enemy.getATK() + ", DEF:" + enemy.getDEF() + ")");
+                showHP(enemy.getHP(), enemy.getMaxHP());
+                System.out.println("--------------------");
+                System.out.println(
+                        "Player HP: " + player.getHP() + " (ATK:" + player.getATK() + ", DEF:" + player.getDEF() + ")");
+                showHP(player.getHP(), player.getMaxHP());
+                System.out.println("--------------------");
+                System.out.println("[0]: Attack");
+                System.out.println("[1]: Defence");
+                System.out.println("[2]: Power Up");
+
+                // user action
+                int userAction = -1;
+                while (!userActionValid) {
+                    userAction = scan.nextInt();
+                    System.out.print("\033[1F\33[K");
+                    System.out.flush();
+                    if ((userAction >= 0) && (userAction <= 2)) {
+                        userActionValid = true;
+                        System.out.print("\033[H\033[2J");
+                        System.out.flush();
+                        System.out.println("--------------------");
+                    } else {
+                        System.out.print("\033[1F\33[K");
+                        System.out.flush();
+                        System.out.println("Invalid input!");
+                    }
+                }
+                action(userAction, player, enemy);
+                if ((enemy.getHP() <= 0)) {
+                    break;
+                }
+
+                // enemy action
+                int r = (int) (Math.random() * 3);
+                action(r, enemy, player);
+                if ((player.getHP() <= 0)) {
+                    break;
+                }
+
+                userActionValid = false;
+                userAction = -1;
+                counter++;
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+            System.out.println("Ended!");
+            if (enemy.getHP() <= 0)
+                System.out.println("You win!");
+            else if (player.getHP() <= 0)
+                System.out.println("You lose!");
+            else
+                System.out.println("Error end");
         }
-        System.out.println("Ended!");
-        if (enemy.getHP() <= 0)
-            System.out.println("You win!");
-        else if (player.getHP() <= 0)
-            System.out.println("You lose!");
-        else
-            System.out.println("Error end");
     }
 
     public static void action(int input, Unit from, Unit to) {
